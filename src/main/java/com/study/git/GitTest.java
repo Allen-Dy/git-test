4b9728fb8275651bb45ab35eaa53afcea6fc1aff
Allen-Dy <63438894+Allen-Dy@users.noreply.github.com>
2021-05-16 15:02:00 +0800

    Update GitTest.java
    github pull

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("hello git github222");
         System.out.println("hello git github ssh");
         System.out.println("hello git github ssh222");
+        
+        System.out.println("hello git github pull");
     }
 }
