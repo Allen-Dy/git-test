875e1bb4c4d60e0c2e72d636ad0f716f2c7015a9
Allen-Dy <63438894+Allen-Dy@users.noreply.github.com>
2021-05-16 14:36:31 +0800

    github222 commit

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
 
         System.out.println("hello git hot-fix");
         System.out.println("hello git github");
+        System.out.println("hello git github222");
     }
 }
